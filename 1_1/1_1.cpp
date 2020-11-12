3b4dd6080c071e5539a2bb501465588ec6959f04
Иван Поздеев <pozdeewiwan@outlook.com>
2020-11-12 22:01:17 +0500

    Добавьте файлы проекта.

---
+++ 1_1/1_1.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int a, b, c;
+    cout << "Введите 3 числа: ";
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (a > b && a > c)
+    {
+        cout << "Максимальное число: " << a;
+
+    }
+    else
+    {
+        if (b > a&& b > c)
+        {
+            cout << "Максимальное число: " << b;
+        }
+        else
+        {
+            cout << "Максимальное число: " << c;
+        }
+
+    }
+}
