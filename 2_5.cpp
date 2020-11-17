a50a87b4b875b23c2d7efc4f100423cbef118b0e
Dasha138 <74371426+Dasha138@users.noreply.github.com>
2020-11-17 11:49:44 +0500

    Update 2_5.cpp

--- 2_5.cpp
+++ 2_5.cpp
@@ -1,20 +1,24 @@
-﻿
-#include <iostream>
+#include <iostream> 
 using namespace std;
-
-
+int n;
 int main()
 {
-    int n;
     cin >> n;
-    for (int i = 0; i < n; i++) 
+    for (int i = 1; i <= n; i++)
     {
-        cout << '*';
-        for (int j = 0; j < i; j++) 
+        for (int j = 1; j <= n; j++)
         {
-            cout << "**";
-        }
-        cout << endl;
-    }
-}
+            if (i + j <= n)
+            {
+                cout << ' ';
+            }
+            else
+            {
+                cout << "*";
+            }
 
+        }
+        cout << "\n";
+    }
+    return 0;
+}
