8d55c0e9d78efc87254a081fdb022ed0739e211e
Dasha138 <74371426+Dasha138@users.noreply.github.com>
2020-12-16 15:29:17 +0500

    Add files via upload

---
+++ 2_20.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	float z,n,b;
+	bool  rule = false;
+	cout << "Введите n: ";
+	cin >> n;
+	cout << "Введите z: ";
+	cin >> z;
+	for (int i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			b = sin(n + i / n);
+			cout << b << " ";
+			if (b == z)
+			{
+				rule = true;
+				break;
+			}
+		}
+	}
+	if (rule)
+	{
+		cout << "Да";
+	}
+	else 
+	{
+		cout << "Нет";
+	}
+	
+}
+
---
+++ 2_23.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float max,n,a;
+	int k = 0;
+	cout << "Введите n: ";
+	cin >> n;
+	max = sin(n + 1 / n);
+	for (int i = 1; i <= n; i++)
+	{
+		if (n > 0)
+		{
+			a = sin(n + i / n);
+			cout << a << " ";
+			if (a > max)
+			{
+				max = a;
+			}
+			else
+			{
+				if (a == max)
+				{
+					k++;
+				}
+			}
+		}
+	}
+	cout << endl << "Максимальное значение: " << max ;
+	cout << endl<<"Совпадает с максимальным: " << k;
+	return 0;
+}
